5a36d14087d02833bdac5c9e039c1a2bb23daab2
DilanBernal28 <dilanbernal01@gmail.com>
2025-05-24 13:18:39 -0500

    Actualizar 2

---
+++ src/app/Content/product/detail/coments/coments.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-coments',
+  imports: [],
+  templateUrl: './coments.component.html',
+  styleUrl: './coments.component.css'
+})
+export class ComentsComponent {
+
+}
